af6bb78966264e279b1b32364e09d377f364fb09
DareenH1 <149277671+DareenH1@users.noreply.github.com>
2023-11-12 07:11:57 -0500

    Added a cloud prefab within its scripts
    Not done yer

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,11 +6,13 @@ public class GameManager : MonoBehaviour
 {
     public GameObject enemyOnePrefab;
     public GameObject enemyTwoPrefab;
+    public GameObject cloudPrefab;
     // Start is called before the first frame update
     void Start()
     {
         InvokeRepeating("CreateEnemyOne", 1.0f, 3.0f);
         InvokeRepeating("CreateEnemyTwo", 3.0f, 2.0f);
+        CreateSky();
     }
 
     // Update is called once per frame
@@ -27,5 +29,13 @@ public class GameManager : MonoBehaviour
     void CreateEnemyTwo()
     {
         Instantiate(enemyTwoPrefab, new Vector3(Random.Range(8, -8), -7, 0), Quaternion.identity);
+    }
+    void CreateSky()
+    {
+        for (int i = 0; i < 50; i++)
+        {
+            Instantiate(cloudPrefab, new Vector3(Random.Range(-11f, 11), Random.Range(-7.5f, 8), 0), Quaternion.identity);
+        }
+        
     }
 }
